e6d62d2bab9614b5124c311c2a039447b519bf81
100rab <saurabhkaundal94@gamil.com>
2022-06-06 14:58:58 +0530

    redux_cart

---
+++ src/components/CardsData.js
@@ -0,0 +1,94 @@
+const Cardsdata = [
+    {
+        id: 1,
+        rname: "Backpack",
+        imgdata: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+        address: "Ladies bag in blue",
+        somedata: "Your perfect pack for everyday use and walks in the forest. Stash your laptop in the padded sleeve, your everyday",
+        price: 109.95,
+        rating: "3.8",
+        qnty:0
+    },
+    {
+        id: 2,
+        rname: "Shirt",
+        imgdata: "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+        address: "Black and white shirt",
+        somedata: "Slim-fitting style, contrast raglan long sleeve",
+        price: 25,
+        rating: "3.9",
+         qnty:0
+    },
+    {
+        id: 3,
+        rname: "Mens Cotton Jacket",
+        imgdata: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+        address: "Grey jaket",
+        somedata: "great outerwear jackets for Spring/Autumn/Winter",
+        price: 55.99,
+        rating: "4.2",
+         qnty:0
+    },
+    {
+        id: 4,
+        rname: "Mens Casual Slim Fit",
+        imgdata: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+        address: "Slimt fit blue shirt",
+        somedata: "The color could be slightly different between on the screen and in practice",
+        price: 40,
+        rating: "3.8",
+         qnty:0
+    },
+    {
+        id: 5,
+        rname: "Bracelet",
+        imgdata: "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
+        address: "Sliver dragon bracelet",
+        somedata: "The Naga was inspired by the mythical water dragon that protects the ocean's pearl",
+        price: 210,
+        rating: "4.0",
+         qnty:0
+    },
+    {
+        id: 6,
+        rname: "Solid Gold Petite Micropave",
+        imgdata: "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
+        address: "Sliver Princess bracelet",
+        somedata: "Sliver shining bracelet",
+        price: 100,
+        rating: "3.8",
+         qnty:0
+    },
+    {
+        id: 7,
+        rname: "White Gold Plated",
+        imgdata: "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg",
+        address: "Silver ring ",
+        somedata: "Pure sliver with shining diamond",
+        price: 300,
+        rating: "3.8",
+         qnty:0
+    },
+    {
+        id: 8,
+        rname: "External Hard Drive",
+        imgdata: "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg",
+        address: "1 TB hd harddrive",
+        somedata: "Compatibility may vary depending on user’s hardware configuration",
+        price: 100,
+        rating: "3.2",
+         qnty:0
+    },
+    {
+        id: 9,
+        rname: "SanDisk SSD",
+        imgdata: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+        address: "32 Gb memory card",
+        somedata: "The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s",
+        price: 100,
+        rating: "3.8",
+         qnty:0
+    },
+];
+
+export default Cardsdata;
